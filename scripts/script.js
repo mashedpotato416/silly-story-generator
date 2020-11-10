5b32eb227d4f2e18257808c0600cbcf017013ce8
Hector Vergara <hector.vergara416@gmail.com>
2020-11-10 20:17:13 +0800

    feat: add us/uk feature script
    Added scripts that checks if which of the radio button is currently
    checked. Once 'generate random story' is checked, temperature and weight
    is now dependent on the button checked before clicking. Provided
    some comments in the code.

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,51 @@
+// Get a random number between 0-2
 function getRandomNumber() {
   return Math.floor(Math.random()*3)
 }
 
+// Get random word from listOfWords
 function getRandomWord(listOfWords) {
   return listOfWords[getRandomNumber()]
 }
 
+// Creates a random story
 function getStory() {
-  let output = document.querySelector('.story')
-  let customName = document.querySelector('#customname').value 
 
-  let whoUser = 'Bob'
-  if (customName !== "") {
-    whoUser = customName
-  }
+  // Initialize variables and get data from html
+  let output = document.querySelector('.story')
+  let customName = document.querySelector('#customname').value
+  let ukButtonStatus = document.querySelector('#uk').checked
+  let whoUser
+  let valuesPhrase
   let whoPhrase = getRandomWord(['Willy the Goblin','Big Daddy','Father Christmas'])
   let wherePhrase = getRandomWord(['the soup kitchen','Disneyland','the White House'])
   let whatPhrase = getRandomWord(['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'])
 
-  output.textContent = `It was 94 fahrenheit outside, so ${ whoPhrase } went for a walk. 
+  // Checks if user entered a custom name
+  if (customName !== "") {
+    whoUser = customName
+  } else {
+    whoUser = 'Bob'
+  }
+
+  // Checks if which button is checked
+  if (ukButtonStatus === false) {  
+    valuesPhrase = ['94 fahrenheit','300 pounds']
+  } else {
+    valuesPhrase = ['34 centigrade','21 stone']
+  }
+
+  // Create output based on user input and default values
+  output.textContent = `It was ${ valuesPhrase[0] } outside, so ${ whoPhrase } went for a walk. 
   When they got to ${ wherePhrase }, they stared in horror for a few moments,
   then ${ whatPhrase }. ${ whoUser } saw the whole thing, but was not surprised
-    — ${ whoPhrase } weighs 300 pounds, and it was a hot day.`
+    — ${ whoPhrase } weighs ${ valuesPhrase[1] }, and it was a hot day.`
 
+  // Change visibility
   output.style.visibility = 'visible'
 }
 
+// Event listener for user click on "Generate random story" button
 randomButton = document.querySelector('.randomize')
 randomButton.addEventListener('click',getStory)
 
