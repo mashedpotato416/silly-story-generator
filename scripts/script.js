d2c1a7527c0b6d2edc0bbfd35a07a0a4a6634092
Hector Vergara <hector.vergara416@gmail.com>
2020-11-10 19:55:20 +0800

    feat: add scripts for custom name feature

--- scripts/script.js
+++ scripts/script.js
@@ -8,14 +8,19 @@ function getRandomWord(listOfWords) {
 
 function getStory() {
   let output = document.querySelector('.story')
+  let customName = document.querySelector('#customname').value 
 
+  let whoUser = 'Bob'
+  if (customName !== "") {
+    whoUser = customName
+  }
   let whoPhrase = getRandomWord(['Willy the Goblin','Big Daddy','Father Christmas'])
   let wherePhrase = getRandomWord(['the soup kitchen','Disneyland','the White House'])
   let whatPhrase = getRandomWord(['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'])
 
   output.textContent = `It was 94 fahrenheit outside, so ${ whoPhrase } went for a walk. 
   When they got to ${ wherePhrase }, they stared in horror for a few moments,
-  then ${ whatPhrase }. Bob saw the whole thing, but was not surprised
+  then ${ whatPhrase }. ${ whoUser } saw the whole thing, but was not surprised
     — ${ whoPhrase } weighs 300 pounds, and it was a hot day.`
 
   output.style.visibility = 'visible'
